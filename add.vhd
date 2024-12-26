648094b81f9417970ee378ded1d8cc7b886188c5
Charran Thangeswaran <126629370+cthanges@users.noreply.github.com>
2024-12-25 22:58:30 -0500

    Update add.vhd

--- add.vhd
+++ add.vhd
@@ -4,8 +4,9 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity add is
-port (A   : in std_logic_vector(31 downto 0);
-B   : out std_logic_vector(31 downto 0)
+port (
+      A : in std_logic_vector(31 downto 0);
+      B : out std_logic_vector(31 downto 0)
 );
 end add;
 
