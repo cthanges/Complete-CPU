8f1d12ff95ce6f0615ce060e93ee81cb88389376
Charran Thangeswaran <126629370+cthanges@users.noreply.github.com>
2024-12-25 22:34:25 -0500

    Update mux2to1.vhd

--- mux2to1.vhd
+++ mux2to1.vhd
@@ -3,8 +3,8 @@ use ieee.std_logic_1164.all;
 
 entity mux2to1 is
 port (s   :  in std_logic;
-w0, w1 :  in std_logic_vector(31 downto 0);
-f      :  out std_logic_vector(31 downto 0)
+w0, w1    :  in std_logic_vector(31 downto 0);
+f         :  out std_logic_vector(31 downto 0)
 );
 end mux2to1;
 
