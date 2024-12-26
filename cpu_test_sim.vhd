46d31894c56e23fc5e64019dbc221f4f8eb3eaff
Charran Thangeswaran <126629370+cthanges@users.noreply.github.com>
2024-12-25 22:42:09 -0500

    Update cpu_test_sim.vhd

--- cpu_test_sim.vhd
+++ cpu_test_sim.vhd
@@ -3,44 +3,49 @@ use ieee.std_logic_1164.all;
 
 ENTITY CPU_TEST_Sim IS
 	PORT(
+		-- Inputs
 		cpuClk : in std_logic;
 		memClk : in std_logic;
 		rst : in std_logic;
-		-- Debug data.
+		
+		-- Debug data
 		outA, outB : out std_logic_vector(31 downto 0);
 		outC, outZ : out std_logic;
 		outIR : out std_logic_vector(31 downto 0);
 		outPC : out std_logic_vector(31 downto 0);
-		-- Processor-Inst Memory Interface.
+		
+		-- Processor-Inst Memory Interface
 		addrOut : out std_logic_vector(5 downto 0);
 		wEn : out std_logic;
 		memDataOut : out std_logic_vector(31 downto 0);
 		memDataIn : out std_logic_vector(31 downto 0);
+		
 		-- Processor State
 		T_Info : out std_logic_vector(2 downto 0);
-		--data Memory Interface
+		
+		-- Data Memory Interface
 		wen_mem, en_mem : out std_logic);
 END CPU_TEST_Sim;
 
 ARCHITECTURE behavior OF CPU_TEST_Sim IS
 	COMPONENT system_memory
 		PORT(
-			address 	: IN 	STD_LOGIC_VECTOR (5 DOWNTO 0);
-			clock 	: IN 	STD_LOGIC ;
-			data 		: IN 	STD_LOGIC_VECTOR (31 DOWNTO 0);
-			wren 		: IN 	STD_LOGIC ;
-			q 			: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
+			address 	: IN STD_LOGIC_VECTOR (5 DOWNTO 0);
+			clock 	        : IN STD_LOGIC ;
+			data 		: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+			wren 		: IN STD_LOGIC ;
+			q 		: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
  );
  END COMPONENT;
  
 	COMPONENT cpu1
 		PORT(
-			clk 		: in std_logic;
-			mem_clk 	: in std_logic;
-			rst 		: in std_logic;
-			dataIn 	: in std_logic_vector(31 downto 0); 
-			dataOut 	: out std_logic_vector(31 downto 0);
-			addrOut 	: out std_logic_vector(31 downto 0);
+			clk : in std_logic;
+			mem_clk : in std_logic;
+			rst : in std_logic;
+			dataIn : in std_logic_vector(31 downto 0); 
+			dataOut : out std_logic_vector(31 downto 0);
+			addrOut : out std_logic_vector(31 downto 0);
 			wEn : out std_logic;
 			dOutA, dOutB : out std_logic_vector(31 downto 0);
 			dOutC, dOutZ : out std_logic;
@@ -56,7 +61,7 @@ ARCHITECTURE behavior OF CPU_TEST_Sim IS
 	signal wen_from_cpu: std_logic;
 
 BEGIN
- -- Component instantiations.
+ -- Component instantiations
 	main_memory : system_memory
 		PORT MAP(
 			 address => add_from_cpu(5 downto 0),
