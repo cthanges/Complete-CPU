ad3f9d7863e8157027ff7efd969fd22fd57a6ec2
Charran Thangeswaran <126629370+cthanges@users.noreply.github.com>
2024-12-25 22:56:12 -0500

    Update adder32.vhd

--- adder32.vhd
+++ adder32.vhd
@@ -5,8 +5,8 @@ entity adder32 is
 port(
 		Cin    :  in std_logic;
 		X,Y    :  in std_logic_vector(31 downto 0);
-		S    :  out  std_logic_vector(31 downto 0);
-		Cout :  out  std_logic
+		S      :  out  std_logic_vector(31 downto 0);
+		Cout   :  out  std_logic
 		);
 end adder32;
 
@@ -15,13 +15,13 @@ architecture Behaviour of adder32 is
 	port(
 		Cin    :  in std_logic;
 		X,Y    :  in std_logic_vector(15 downto 0);
-		S    :  out  std_logic_vector(15 downto 0);
-		Cout :  out  std_logic
+		S      :  out  std_logic_vector(15 downto 0);
+		Cout   :  out  std_logic
 	);
 	end component;
 	
 	signal C : std_logic;
 begin
-	stage0: adder16 port map(Cin,  X(15 downto 0), Y(15 downto 0), S(15 downto 0), C);
-	stage1: adder16 port map(C, X(31 downto 16), Y(31 downto 16), S(31 downto 16), Cout);
+	stage0 : adder16 port map(Cin,  X(15 downto 0), Y(15 downto 0), S(15 downto 0), C);
+	stage1 : adder16 port map(C, X(31 downto 16), Y(31 downto 16), S(31 downto 16), Cout);
 end Behaviour;
